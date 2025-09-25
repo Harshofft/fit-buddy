55145914cb5dfccf1bd53133e322cd5ac97d29b7
Harshofft <hdharsh122@gmail.com>
2025-09-25 20:26:14 +0530

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React , {useState} from 'react'
+import {Box} from '@mui/material'
+import HeroBanner from '../components/HeroBanner'
+import SearchExercise from '../components/SearchExercise'
+import Exercises from '../components/Exercises'
+function Home() {
+    const [exercises, setExercises] = useState([])
+    const [bodyPart, setBodyPart] = useState('all')
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercise setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      <Exercises  setExercises={setExercises} bodyPart={bodyPart} exercises={exercises}/>
+    </Box>
+  )
+}
+
+export default Home
---
+++ src/utils/fetchdata.jsx
@@ -0,0 +1,21 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': "5c09e14bb7msh7decdfd791da0a0p195052jsn6d4e46403869",
+  },
+};
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
